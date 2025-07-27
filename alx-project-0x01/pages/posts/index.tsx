52e43d436a2b9410809c2f3a89110e7858c02eae
Zakesde-prince <prince.feela2905@gmail.com>
2025-07-27 15:39:02 +0200

    .

--- alx-project-0x01/pages/posts/index.tsx
+++ alx-project-0x01/pages/posts/index.tsx
@@ -1,5 +1,14 @@
+import Header from "@/components/layout/Header";
+
 const PostsPage = () => {
-  return <div className="p-4">All Posts</div>;
+  return (
+    <div className="flex flex-col min-h-screen">
+      <Header />
+      <main className="flex-grow p-4 bg-gray-100">
+        <h1 className="text-3xl font-bold">All Posts</h1>
+      </main>
+    </div>
+  );
 };
 
 export default PostsPage;
--- alx-project-0x01/pages/users/index.tsx
+++ alx-project-0x01/pages/users/index.tsx
@@ -1,5 +1,14 @@
+import Header from "@/components/layout/Header";
+
 const UsersPage = () => {
-  return <div className="p-4">All Users</div>;
+  return (
+    <div className="flex flex-col min-h-screen">
+      <Header />
+      <main className="flex-grow p-4 bg-gray-100">
+        <h1 className="text-3xl font-bold">All Users</h1>
+      </main>
+    </div>
+  );
 };
 
 export default UsersPage;
