ba54b9b83b0464e30a4da1f3391315aa05f0980d
Zakesde-prince <prince.feela2905@gmail.com>
2025-07-27 15:21:52 +0200

    .

---
+++ alx-project-0x01/components/common/PostCard.tsx
@@ -0,0 +1,5 @@
+const PostCard = () => {
+  return <div className="p-4 border rounded shadow">This is a post</div>;
+};
+
+export default PostCard;
---
+++ alx-project-0x01/components/layout/Header.tsx
@@ -0,0 +1,5 @@
+const Header = () => {
+  return <header className="p-4 bg-gray-800 text-white">Header</header>;
+};
+
+export default Header;
